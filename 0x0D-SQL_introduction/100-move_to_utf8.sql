5928021e73f31f32db6cad4d4c5ec3716905acf3
abdulnasirym <abdulnasiry03@gmail.com>
2023-05-27 11:53:59 -0700

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,2 @@
 -- convert database,table and field to UTF8
-USE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE DATABASE hbtn_0c_0 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
