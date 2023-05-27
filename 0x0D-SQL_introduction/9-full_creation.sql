dc6e0094a895b293f446020c04f974a27620d40b
abdulnasirym <abdulnasiry03@gmail.com>
2023-05-27 04:43:21 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- script that create a table second_table in the mySQL server
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES(1, "john", 10));
+INSERT INTO second_table VALUES(1, "john", 10);
 INSERT INTO second_table VALUES(2, "Alex", 3);
 INSERT INTO second_table VALUES(3, "Bob", 14);
-INSERT INTO second_table VALUES(4, "George", 8;
+INSERT INTO second_table VALUES(4, "George", 8);
