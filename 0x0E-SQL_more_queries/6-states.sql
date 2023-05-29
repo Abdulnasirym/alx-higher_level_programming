b726fa017bc8faf25eeaf65572c19237e22992d2
abdulnasirym <abdulnasiry03@gmail.com>
2023-05-29 08:25:22 -0700

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- creates database hbtn_0d_usa and table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE HBTN_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
