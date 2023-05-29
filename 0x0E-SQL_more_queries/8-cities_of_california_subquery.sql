0569ba0acd023cf85396a539b66c90d533e548e1
abdulnasirym <abdulnasiry03@gmail.com>
2023-05-29 08:11:16 -0700

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- list cities of california that can be found in the database hbtn_0d_usa
-SELECT id, name FROM cities WHERE id=1 ORDER BY cities.id;
+SELECT id, name FROM cities WHERE state_id=1 ORDER BY cities.id;
