e34712510bbe8a0f67b7c8fe6e6ba4299b69af93
abdulnasirym <abdulnasiry03@gmail.com>
2023-05-30 10:05:10 -0700

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- list all cities contained in the database hbtn_0d_usa
-SELECT cities.id , cities.name, states.name FROM hbtn_0d_usa ORDER BY cities.id;
+SELECT cities.id , cities.name , states.name
+FROM hbtn_0d_usa.cities JOIN hbtn_0d_usa.states ON cities.id=states.id;
