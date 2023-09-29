da63c680c26fec7e9a4e4400e69d62a8c6c6ce47
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2023-09-29 20:19:38 +0100

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print("{:s}".format(chr(i)), end="")
+start = ord('a')
+stop = ord('z') + 1
+for i in range(start, stop):
+    print("{:c}".format(i), end='')
