b7ebe3277e2e110a6a0d03ee37952c3633a7c578
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2023-09-29 19:48:02 +0100

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-start = ord('a')
-stop = ord('z') + 1
+i = ord('a')
 
-my_range = range(start, stop)
-for i in my_range:
+while i <= ord('z'):
     print(chr(i))
-print('\n')
+    i = i + 1
