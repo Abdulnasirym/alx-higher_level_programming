f7d060930cd7cc8078a40146204317d3379b2b25
abdulnasirym <abdulnasiry05@gmail.com>
2023-12-02 15:12:00 -0800

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,6 @@
 def replace_in_list(my_list, idx, element):
     size = len(my_list)
     if idx < 0 and idx >= size:
-        return (my_list)
-    my_list.pop(idx)
-    return (my_list.insert(idx, element))
+        return my_list
+    my_list[idx] = element
+    return my_list
