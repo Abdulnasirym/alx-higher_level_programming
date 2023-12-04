e42315ea9b5e5eddf24c389e01325f4e334335b9
abdulnasirym <abdulnasiry05@gmail.com>
2023-12-04 15:27:39 -0800

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     size = len(sentence)
-    if size == 0:
-        sentence[0] = ""
-    return (size, sentence[0])
+    if size > 0:
+        first_char = sentence[0]
+    else:
+        first_char = "None"
+    return (size, first_char)
