cb1f20312903f0009938e16b92aff742f8703204
abdulnasirym <abdulnasiry05@gmail.com>
2023-12-14 15:38:07 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@
 def square_matrix_simple(matrix=[]):
     new_array = []
     for lists in matrix:
-        result =list(map(lambda x: x ** 2, lists))
+        result = list(map(lambda x: x ** 2, lists))
         new_array.append(result)
     return new_array
