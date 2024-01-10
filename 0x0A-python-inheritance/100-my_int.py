437f557b2f46ed1c53897e3bb596fea9f12d337b
abdulnasirym <abdulnasiry05@gmail.com>
2024-01-10 07:19:26 -0800

    task 12

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,10 +3,12 @@
 
 
 class MyInt(int):
+    """Invert int operators == and !=."""
+
     def __eq__(self, other):
-        # Override the == operator
+        """Override the == operator with != behavior."""
         return super().__ne__(other)
 
     def __ne__(self, other):
-        # Override the != operator
+        """Override the != operator with behaviour."""
         return super().__eq__(other)
