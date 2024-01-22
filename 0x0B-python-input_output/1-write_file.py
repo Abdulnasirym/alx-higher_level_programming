195274d60e2cd60e3c8d7df619e2bf4cc0c589a5
abdulnasirym <abdulnasiry05@gmail.com>
2024-01-22 02:49:27 -0800

    task1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function taht writes to a file"""
+
+def write_file(filename="", text=""):
+    """write a content to a file"""
+    with open(filename, 'w', encoding="utf-8") as f:
+        f_content = f.read()
+        print(f_content, end="")
