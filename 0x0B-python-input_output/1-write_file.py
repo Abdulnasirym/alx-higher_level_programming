dd487b298e6e3b564a3122cdd113fd04aa48a3ea
abdulnasirym <abdulnasiry05@gmail.com>
2024-01-22 02:51:55 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 def write_file(filename="", text=""):
     """write a content to a file"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f_content = f.write()
+        f_content = f.write(text)
         print(f_content, end="")
