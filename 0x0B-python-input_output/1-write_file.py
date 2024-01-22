4f569aaef2e3e2dc621abf9c7021a169d5531d23
abdulnasirym <abdulnasiry05@gmail.com>
2024-01-22 03:00:16 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,13 @@
 """Defines a function taht writes to a file"""
 
 def write_file(filename="", text=""):
-    """write a content to a file"""
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
     with open(filename, 'w', encoding="utf-8") as f:
-        return (f.write(text))
+        return f.write(text)
