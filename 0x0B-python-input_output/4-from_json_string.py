7664ca09ef0971fd6f28d6908993ecf7fc74c81a
abdulnasirym <abdulnasiry05@gmail.com>
2024-02-16 05:13:46 -0800

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a JSON-to-string function."""
+import Json
+
+
+def from_json_string(my_str):
+    """
+    Convert a JSON string to a Python object.
+
+    Parameters:
+        my_str (str): JSON string
+
+    Returns:
+        object: Python object represented by the JSON string
+    """
+    return json.loads(my_str)
