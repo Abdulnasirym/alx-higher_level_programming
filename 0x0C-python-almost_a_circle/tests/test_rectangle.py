92e8bd1efb4d20768a10e40814a88a7cfd2f383b
Abdulnasirym <Abdulnasiry05@gmail.com>
2024-02-27 15:12:29 -0800

    task 4

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -14,11 +14,11 @@ class TestRectangle(unittest.TestCase):
         """Test the creation of the Rectangle and the attributes values"""
         rectangle = Rectangle(width=10, height=12, x=3, y=4, id=5)
 
-        self.assertEqual(Rectangle.width, 10)
-        self.assertEqual(Rectangle.height, 12)
-        self.assertEqual(Rectangle.x, 3)
-        self.assertEqual(Rectangle.y, 4)
-        self.assertEqual(Rectangle.id, 5)
+        self.assertEqual(rectangle.width, 10)
+        self.assertEqual(rectangle.height, 12)
+        self.assertEqual(rectangle.x, 3)
+        self.assertEqual(rectangle.y, 4)
+        self.assertEqual(rectangle.id, 5)
 
     def test_setters(self):
         """Test setters and getters"""
@@ -30,7 +30,7 @@ class TestRectangle(unittest.TestCase):
         rectangle.y = 6
 
         self.assertEqual(rectangle.width, 11)
-        self.assertEqual(recatangle.height, 12)
+        self.assertEqual(rectangle.height, 12)
         self.assertEqual(rectangle.x, 7)
         self.assertEqual(rectangle.y, 6)
 
@@ -42,6 +42,11 @@ class TestRectangle(unittest.TestCase):
         rectangle.height = 0
         self.assertEqual(rectangle.width, 15)
         self.assertEqual(retangle.height, 11)
+
+    def test_area(self):
+        """Testing the area of the rectangle"""
+        rectangle = Rectangle(width=5, height=4)
+        self.assertEqual(rectangle.area(), 12)
 
 
 if __name__ == "__main__":
