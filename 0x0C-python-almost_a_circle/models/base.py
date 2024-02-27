849e31a8c47ed31a6ec93f2a79ec927519a2a3e0
Abdulnasirym <Abdulnasiry05@gmail.com>
2024-02-26 17:58:41 -0800

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+class Base:
+    """Base model
+
+    This This is the base of all classes in the "python almost a circle project
+
+    private Clss Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initiate a new base.
+
+        Args:
+            id(int): The id of the new base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+import unittest
+from models.base import Base
+
+class BaseTest(unittest.TestCase):
+
+    def test_instancewith_id(self):
+        """test when id is provided"""
+        obj = Base(id=20)
+        self.assertEqual(obj.id, 20)
+    
+    def test_instancewithout_id(self):
+        """test when id is not provided"""
+        obj1 = Base()
+        obj2 = Base()
+
+        self.assertEqual(obj1.id, 1)
+        self.assertEqual(obj2.id, 2)
+
+if __name__ == "__main__":
+    unittest.main()
