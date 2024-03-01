91810b5c65fe6c13c7d49f8e2e9c4a91fa691af6
Abdulnasirym <Abdulnasiry05@gmail.com>
2024-03-01 14:46:20 -0800

    task 4

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -34,15 +34,52 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rectangle.x, 7)
         self.assertEqual(rectangle.y, 6)
 
+
+"""
     def test_invalid_inputs(self):
-        """Test invalid values entered into the function"""
         rectangle = Rectangle(width=15, height=11, x=9, y=7, id=1)
 
         rectangle.width = -5
         rectangle.height = 0
         self.assertEqual(rectangle.width, 15)
         self.assertEqual(retangle.height, 11)
+"""
+    def test_invalid_inputs(self):
+        rectangle = Rectangle(width=5, height=3, x=2, y=3, id=1)
 
+        with self.assertRaises(TypeError) as context:
+            rectangle.width = "invalid_width"
+        self.assertEqual(str(context.exception), "width must be an integer")
+
+        with self.assertRaises(ValueError) as context:
+            rectangle.width = -5
+        self.assertEqual(str(context.exception), "width must be > 0")
+
+        with self.assertRaises(TypeError) as context:
+            rectangle.height = "invalid_height"
+        self.assertEqual(str(context.exception), "height must be an integer")
+
+        with self.assertRaises(ValueError) as context:
+            rectangle.height = 0
+        self.assertEqual(str(context.exception), "height must be > 0")
+
+        with self.assertRaises(TypeError) as context:
+            rectangle.x = "invalid_x"
+        self.assertEqual(str(context.exception), "x must be an integer")
+
+        with self.assertRaises(ValueError) as context:
+            rectangle.x = -1
+        self.assertEqual(str(context.exception), "x must be >= 0")
+
+        with self.assertRaises(TypeError) as context:
+            rectangle.y = "invalid_y"
+        self.assertEqual(str(context.exception), "y must be an integer")
+
+        with self.assertRaises(ValueError) as context:
+            rectangle.y = -2
+        self.assertEqual(str(context.exception), "y must be >= 0")
+
+"""
     def test_width(self):
         """Test for invalid width"""
         rectangle = Rectangle(width=10, height=5, x=2, y=3)
@@ -66,7 +103,7 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError) as context:
             rectangle.height = 0
         self.assertEqual(str(context.exception), "height must be > 0")
-
+"""
 
 if __name__ == "__main__":
     unittest.main()
