48b085185890b67464b42fec64405468d256335f
abdulnasirym <abdulnasiry05@gmailcom>
2024-06-08 11:02:18 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const arg = process.argv;
-for (let i=2; i <= arg.length - 1; i++) {
-  console.log(arg[i]);
+for (const line of ['Cis fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
