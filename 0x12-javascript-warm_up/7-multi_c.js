8cbc58bd0d5c17bc449e06548a56ee263f0af7e6
abdulnasirym <abdulnasiry05@gmailcom>
2024-06-09 07:47:17 -0700

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 const value = process.argv[2];
 const intValue = parseInt(value);
 
-if (isNaN(intValue)) {
+if (!intValue) {
   console.log("Missing number of occurences");
 } else {
-  for (let i = 0; i < value; i++) {
+  for (let i = 0; i < intValue; i++) {
     console.log("C is fun");
   }
 }
