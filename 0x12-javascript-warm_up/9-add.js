bc4f9f6df161099198db58a0fe2d2a19a39680f5
abdulnasirym <abdulnasiry05@gmailcom>
2024-06-09 08:53:56 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
 function add(a, b) {
-  let sum = parseInt(process.argv[2]) + parseInt(process.argv[3]);
-  console.log(sum);
+  return (a + b);
 }
-add();
+
+console.log(add(a, b));
