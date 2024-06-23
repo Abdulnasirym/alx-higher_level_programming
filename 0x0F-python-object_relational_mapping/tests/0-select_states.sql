369dbb990a62f6cb656780c4f5e5d560a52fda74
abdulnasirym <abdulnasiry05@gmail.com>
2024-06-23 14:02:04 -0700

    task 0

---
+++ 0x0F-python-object_relational_mapping/tests/0-select_states.sql
@@ -0,0 +1,11 @@
+-- creates states table in hbtn
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+
+CREATE TABLE IF NOT EXISTS States (
+	id INT NOT NULL AUTO_INCREMENT;
+	name VARCHAR(256) NOT NULL;
+	PRIMARY KEY (id)
+);
+
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
