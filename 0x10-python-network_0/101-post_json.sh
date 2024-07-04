da7c2bac82e4c3300401b27b7d26e711d3250ab7
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-04 15:51:27 -0700

    task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a JSON POST request to URL
-curl -sX POST -H "Content-Type: application/json" -d "$2" "$1" 
+curl -sX POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1" 
