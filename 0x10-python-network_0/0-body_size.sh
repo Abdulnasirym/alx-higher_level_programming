0d6fe8906a7c4de235826bb5c422300d55219d9b
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-04 09:37:53 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,2 @@
-#!/usr/bin/bash
-
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 URL"
-	exit 1
-fi
-
-url=$1
-
-response=$(curl -sI "$url")
-content_length=$(echo "$response" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$content_length" ]; then
-	content_length=$(curl -s "$url" | wc -c)
-fi
-
-echo "Size of the body of the response: $content_length bytes"
+#!/bin/bash
+curl -s "$1" | wc -c
