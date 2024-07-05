7e7bba16b11c2282031f1e0f9e97cc02067887f3
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-05 14:20:44 -0700

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Sends a request to the URL"""
+import urllib.request
+import urllib.error
+import sys
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as res:
+        print(res.read().decode("utf-8"))
+except urllib.error.HTTPError as e:
+    print("Error code: ", e.code )
