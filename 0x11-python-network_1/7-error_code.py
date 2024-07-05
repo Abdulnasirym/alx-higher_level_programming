cca0766c70054ad7a890660ac12d1d0415a032ff
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-05 16:17:58 -0700

    tasl 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,6 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    response = requests.get(sys.argv[1])
+    url = sys.argv[1]
+
+    response = requests.get(url)
     if (response.status_code >= 400):
         print("Error code: ", response.status_code)
