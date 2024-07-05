f04fb17c62fc09a1a91014fe54e81e6e24fc5206
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-05 14:47:48 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,9 +8,9 @@ if __name__=="__main__":
 
 
     url = sys.argv[1]
-
     try:
         with request.urlopen(url) as res:
             print(res.read().decode("utf-8"))
     except error.HTTPError as e:
         print("Error code:", e.code )
+
