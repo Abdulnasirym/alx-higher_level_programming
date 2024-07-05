65089fa696f2ee875aa11405186b0b3c1ab7d22b
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-05 14:40:06 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ Sends a request to the URL"""
-import urllib.request
-import urllib.error
-import sys
 
-
-url = sys.argv[1]
 
 if __name__=="__main__":
+    from urllib import request, error
+    import sys
+
+
+    url = sys.argv[1]
+
     try:
-        with urllib.request.urlopen(url) as res:
+        with request.urlopen(url) as res:
             print(res.read().decode("utf-8"))
-    except urllib.error.HTTPError as e:
-        print("Error code: ", e.code )
+    except error.HTTPError as e:
+        print("Error code:", e.code )
