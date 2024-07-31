7b7827c9a881c0ed8180c7e258dde6af10e47a4f
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-31 09:26:11 -0700

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs')
 const content = process.argv[3];
-fs.writeFile(process.argv[2], content, 'utf8', function (error) {
+fs.writeFile(process.argv[2], content, 'utf8', function (error, content) {
   console.log(error || content);
 });
