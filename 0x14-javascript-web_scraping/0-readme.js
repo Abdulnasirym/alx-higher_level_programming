96d0e61e195467087ef060cd45b78f924950dbce
abdulnasirym <abdulnasiry05@gmail.com>
2024-07-31 07:40:28 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 // A script that reads and prints the content of a file
 
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function(error, content) {
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
 	console.log(error || content);
 });
